21d0580679f922c0475fc7ddb76a38b7db4a447e
Rubén López Gómez <loslopez1@icloud.com>
2016-05-11 21:01:01 +0200

    update: unit test specs

--- test/spec-templatefy.js
+++ test/spec-templatefy.js
@@ -10,7 +10,10 @@ const
     templatefy = function() {
         var args = Array.prototype.slice.call(arguments),
             cb = args.splice(-1)[0];
-        return execFile(cmd, args, cb);
+
+        return args[0] && args[0].join
+              ? exec([args[0].concat([cmd]).join('|')].concat(args.slice(1)).join(' '), cb)
+              : exec([cmd].concat(args).join(' '), cb);
     };
 
 describe('Templatefy', function() {
@@ -46,22 +49,39 @@ describe('Templatefy', function() {
             });
         });
 
-        // it('should require input argument if not pipe stdin defined', function(done) {
-        //     this.timeout(15000);
-        //     templatefy(function(error, stdout, stderr) {
-        //         console.log(error, stdout, stderr);
-        //         done();
-        //     });
+        // it('should require --input argument if not pipe stdin defined', function(done) {
+        //    templatefy(function(error, stdout, stderr) {
+        //        done();
+        //    });
         // });
 
-        // it('should require input argument if not pipe stdin defined', function(done) {
-        //     this.timeout(15000);
-        //     templatefy('--input=<h1>Test</h1>', function(error, stdout, stderr) {
-        //         expect(stdout).to.have.string('\'<h1>Test</h1>\'');
-        //         //console.log(stdout, stderr);
-        //         done();
-        //     });
-        // });
+        it('should print the string template by input parameter to parsed through stdout', function(done) {
+            templatefy('--input=\'<h1  title = "test">Test</h1>\'', function(error, stdout, stderr) {
+                expect(stdout).to.have.string('<h1 title="test">Test</h1>');
+                done();
+            });
+        });
+
+        it('should print the path of template by input parameter to parsed through stdout', function(done) {
+            templatefy('--input=\'./test/fixtures/template-element.html\'', function(error, stdout, stderr) {
+                expect(stdout).to.have.string('<h1 title="test">Test</h1>');
+                done();
+            });
+        });
+
+        it('should print the string template by stdin pipe to parsed through stdout', function(done) {
+            templatefy(['echo \'<h1  title = "test">Test</h1>\''], function(error, stdout, stderr) {
+                expect(stdout).to.have.string('<h1 title="test">Test</h1>');
+                done();
+            });
+        });
+
+        it('should print the string template by stdin file redirection to parsed through stdout', function(done) {
+            templatefy('< ./test/fixtures/template-element.html', function(error, stdout, stderr) {
+                expect(stdout).to.have.string('<h1 title="test">Test</h1>');
+                done();
+            });
+        });
     });
 
     describe('#api', function() {
