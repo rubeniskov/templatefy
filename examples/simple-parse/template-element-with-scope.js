54997cf9853316c19086720bcb21d98ccf46a93c
Rubén López Gómez <loslopez1@icloud.com>
2016-05-21 05:44:41 +0200

    fix: angular parser when global or var options

---
+++ examples/simple-parse/template-element-with-scope.js
@@ -0,0 +1,3 @@
+!(function(global, module){
+      '<h1>Templatefied!</h1>'
+})(typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {}, module || {})
