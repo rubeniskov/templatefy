5caa857d17256c4541ff91983e403f59d0b3024c
Rubén López Gómez <loslopez1@icloud.com>
2016-05-21 04:51:07 +0200

    add: examples folder

---
+++ examples/simple-parse/simple-parse.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+      path = require('path'),
+      Templatefy = require('../..');
+
+Templatefy.parse('./template-element.html', './template-element.js');
+
+
+Templatefy.parse({
+    var: 'exampleVar'
+},'./template-element.html', './template-element-with-var.js');
+
+Templatefy.parse({
+    global: 'exampleGlobal'
+},'./template-element.html', './template-element-with-global.js');
+
+Templatefy.parse({
+    exports: true
+},'./template-element.html', './template-element-with-exports.js');
+
+Templatefy.parse({
+    scope: true
+},'./template-element.html', './template-element-with-scope.js');
