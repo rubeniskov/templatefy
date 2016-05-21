edd854552101843dd4cfcafc273a65607b7c6f00
Rubén López Gómez <loslopez1@icloud.com>
2016-05-21 05:45:41 +0200

    update: example ouput

---
+++ examples/simple-parse/template-element-with-full.js
@@ -0,0 +1,6 @@
+!(function(global, module){
+      var exampleVar = module.exports = exampleGlobalVarName['exampleGlobalPropertyName'] = '<h1>Templatefied!</h1>';
+;
+;
+
+})(typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {}, module || {})
